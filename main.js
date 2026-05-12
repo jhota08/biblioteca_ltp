47503cc7d260b6485892c84c01a5656afabd7fe0
jhota08 <batistajuliaoj@gmail.com>
2026-05-11 22:01:07 -0400

    adicionar menu em loop

--- main.js
+++ main.js
@@ -2,4 +2,17 @@ const prompt = require("prompt-sync")();
 const Livro = require("./Livro");
 
 const livros = [];
+let opcao = -1;
+
+while(opcao !==0){
+    console.log("\n--- BIBLIOTECA ---");
+    console.log("1 - Cadastrar livro");
+    console.log("2 - Listar livros");
+    console.log("3 - Alterar livro");
+    console.log("4 - Estatísticas");
+    console.log("0 - Sair");
+
+    opcao = parseInt(prompt("Opção: "));
+
+    }
 
